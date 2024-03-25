33cc823d5129157eba5059e55b4d70759e007930
Farouk-ayo <mustaphafarouk41@gmail.com>
2024-03-25 14:54:58 +0100

    done

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,4 +1,15 @@
+import classes from "./Account.module.scss";
+
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <div>
+      <iframe
+        width="100%"
+        height="500"
+        className={classes.trailer}
+        src={`172.20.10.5.`}
+      ></iframe>
+    </div>
+  );
 };
 export default Account;
